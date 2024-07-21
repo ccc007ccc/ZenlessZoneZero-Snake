1a295e79075469545698342dcc1501f832cd4883
ccc007 <1492949083@qq.com>
2024-07-22 07:53:05 +0800

    feat(start.py): implement auto-click and improve snake rotation logic
    Add new functionality to the start.py script including:
    - Implementation of an auto-click feature using pyautogui.
    - Enhancement of the snake rotation logic to support a 'fast' mode.
    - Introduction of a timer function to automatically toggle the 'fast' mode.
    - Integration of image recognition to locate and click on specific screen regions.
    
    These changes improve the script's usability and add automated gameplay elements.

--- start.py
+++ start.py
@@ -1,43 +1,112 @@
-import sys
 import keyboard
 import time
 import threading
+import pyautogui as pyg
 
 # 用于表示脚本是否应该继续运行
+fast = False
 continue_running = False
+timer_thread = None
+timer_lock = threading.Lock()
+rounds = 0
 
-# 预设游戏键位
-up = 'w'
-down = 's'
-left = 'a'
-right = 'd'
+
+#去左上角 
+def go_to_top_left():
+    global continue_running
+    keyboard.press('a')
+    time.sleep(1.532)
+    keyboard.release('a')
     
+    keyboard.press('w')
+    time.sleep(3.832)
+    keyboard.release('w')
+    
+    continue_running = True    
 # 控制蛇转圈刷分
 def rotate_snake():
-    keyboard.press(right)
-    time.sleep(0.11)
-    keyboard.release(right)
-    keyboard.press(down)
-    time.sleep(0.44)
-    keyboard.release(down)
-    keyboard.press(left)
-    time.sleep(0.11)
-    keyboard.release(left)
-    keyboard.press(up)
-    time.sleep(0.44)
-    keyboard.release(up)
-
-
+    if fast: 
+        keyboard.press('d')
+        time.sleep(0.07)
+        keyboard.release('d')
+        keyboard.press('s')
+        time.sleep(0.179)
+        keyboard.release('s')
+        keyboard.press('a')
+        time.sleep(0.07)
+        keyboard.release('a')
+        keyboard.press('w')
+        time.sleep(0.179)
+        keyboard.release('w')
+        if fast == False:
+            time.sleep(0.261)
+    else:
+        keyboard.press('d')
+        time.sleep(0.07)
+        keyboard.release('d')
+        keyboard.press('s')
+        time.sleep(0.44)
+        keyboard.release('s')
+        keyboard.press('a')
+        time.sleep(0.07)
+        keyboard.release('a')
+        keyboard.press('w')
+        time.sleep(0.44)
+        keyboard.release('w')
 
 def start_script(e):
-    global continue_running
+    global continue_running, timer_thread, fast
     print("脚本开始运行！")
-    continue_running = True
+    # keyboard.press("j")
+    # fast = True
+    # continue_running = True
+    # 启动自动重开
+    threading.Thread(target=click_over_img, daemon=True).start()
+
+    # # 启动计时线程
+    # with timer_lock:
+    #     if timer_thread is None or not timer_thread.is_alive():
+    #         timer_thread = threading.Thread(target=timer)
+    #         timer_thread.start()
 
 def stop_script(e):
     global continue_running
     print("脚本暂停运行！")
+    # keyboard.release("j")
     continue_running = False
+
+def timer():
+    global fast
+    time.sleep(10)
+    keyboard.release("j")
+    fast = False
+    
+
+def click_over_img():
+    global rounds,continue_running
+    height, width = pyg.size()
+    print(height, width)
+    #左上角
+    left_top_x = int(width/3)
+    left_top_y = int(height/3)
+    #右下角
+    right_bottom_x = int(width - width/3)
+    right_bottom_y = int(height - height/3)
+    print(left_top_x, left_top_y, right_bottom_x, right_bottom_y)
+    
+    while True:
+        img = pyg.locateCenterOnScreen("./img/image.png", confidence=0.8, region=(left_top_x, left_top_y, right_bottom_x, right_bottom_y))
+        if img != None:
+            continue_running = False
+            pyg.click(img)
+            time.sleep(1.5)
+            go_to_top_left()
+            rounds += 1
+        elif rounds == 0:
+            continue_running = True
+            rounds = 1 
+                
+        time.sleep(1)
 
 # 当 F10 键被按下时，调用 start_script 函数
 keyboard.on_press_key("f10", start_script)
@@ -50,6 +119,8 @@ def click_loop():
     while True:
         if continue_running:
             rotate_snake()
+        else:
+            time.sleep(0.1)  # 避免空转
 
 # 在一个新的线程中运行 click_loop 函数
 threading.Thread(target=click_loop, daemon=True).start()
