de404215ec5ac14987e7e1ee62218c60da3dd699
ccc007 <1492949083@qq.com>
2024-07-21 23:18:13 +0800

    first commit

---
+++ start.py
@@ -0,0 +1,56 @@
+import keyboard
+import time
+import threading
+
+# 用于表示脚本是否应该继续运行
+continue_running = False
+
+# 预设游戏键位
+up = 'w'
+down = 's'
+left = 'a'
+right = 'd'
+    
+# 控制蛇转圈刷分
+def rotate_snake():
+    keyboard.press(up)
+    time.sleep(0.11)
+    keyboard.release(up)
+    keyboard.press(right)
+    time.sleep(0.11)
+    keyboard.release(right)
+    keyboard.press(down)
+    time.sleep(0.11)
+    keyboard.release(down)
+    keyboard.press(left)
+    time.sleep(0.11)
+    keyboard.release(left)
+
+
+
+def start_script(e):
+    global continue_running
+    print("脚本开始运行！")
+    continue_running = True
+
+def stop_script(e):
+    global continue_running
+    print("脚本暂停运行！")
+    continue_running = False
+
+# 当 F11 键被按下时，调用 start_script 函数
+keyboard.on_press_key("f10", start_script)
+
+# 当 F12 键被按下时，调用 stop_script 函数
+keyboard.on_press_key("f11", stop_script)
+
+def click_loop():
+    # 循环点击，直到 F12 键被按下
+    while True:
+        if continue_running:
+            rotate_snake()
+
+# 在一个新的线程中运行 click_loop 函数
+threading.Thread(target=click_loop, daemon=True).start()
+# 防止脚本退出
+keyboard.wait('f12')
